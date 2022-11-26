716629c9d7e7c25644e8c3f99db6ab1aef4cba7b
JuanTwo779 <104906032+JuanTwo779@users.noreply.github.com>
2022-11-26 13:14:01 +1100

    field changes, new task method

--- UserInterface.java
+++ UserInterface.java
@@ -1,32 +1,56 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserInterface
 {
+
+    private List<Task> tasks;
+    private Input objInput = new Input();
+    private Task newTask;
+
     public UserInterface()
     {
+        tasks = new ArrayList<>();
     }
 
+    //Decide
     //ask user for spare time, mental and physical capacity
-
-    public int time()
+    private int time()
     {
-        Input objInput = new Input();
         int taskTime = 0;
         taskTime = objInput.acceptIntInput("How much time do you have? (In hours)");
         return taskTime;
     }
 
-    public int mentalCap()
+    private int mentalCap()
     {
-        Input objInput = new Input();
         int mentalCap = 0;
         mentalCap = objInput.acceptIntInput("What is your current mental capacity? (1 - 100)");
         return mentalCap;
     }
 
-    public int physicalCap()
+    private int physicalCap()
     {
-        Input objInput = new Input();
         int physicalCap = 0;
         physicalCap = objInput.acceptIntInput("What is your current physical capacity? (1 - 100)");
         return physicalCap;
+    }
+
+    //Add new task
+    private void addTask(){
+        String name = "";
+        String desc = "";
+        Integer time = 0;
+        Integer mental = 0;
+        Integer physical = 0;
+
+        name = objInput.acceptStringInput("Enter to the name of the new task!");
+        desc = objInput.acceptStringInput("Enter a short description of the new task");
+        time = objInput.acceptIntInput("How long (hours) does it take to complete new task");
+        mental = objInput.acceptIntInput("Enter the mental capacity needed for the new task");
+        physical = objInput.acceptIntInput("Enter the physical capacity needed for the new task");
+
+        newTask = new Task(name, desc, time, mental, physical);
+        tasks.add(newTask);
     }
 }
